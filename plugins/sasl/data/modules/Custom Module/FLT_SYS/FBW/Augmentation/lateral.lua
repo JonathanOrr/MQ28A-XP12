c7d2dfa09d24f1dab966574d40999660ffe03929
JonathanOrr <jonathan.orr200299@gmail.com>
2023-03-29 00:54:42 +1100

    [FBW] update roll rate controller for edge cases

--- plugins/sasl/data/modules/Custom Module/FLT_SYS/FBW/Augmentation/lateral.lua
+++ plugins/sasl/data/modules/Custom Module/FLT_SYS/FBW/Augmentation/lateral.lua
@@ -3,6 +3,31 @@ local fphiDotLimit = Lagrange_interpolation(
     {35, 170, 250, 315, 350, 360}
 )
 
+--====input processing====
+local function X_to_P(x)
+    local pitch = get(Flightmodel_pitch)
+    local Plim = fphiDotLimit(Math_clamp(get(TAS_ms) * 1.944, 50, 300))
+    local neutralPComp = get(Flightmodel_p_deg) - PhiDot:deg()
+
+    --pitch is to ensure no strange behaviours while pulling a vertical loop
+    --or pretty much anything invloving extreme pitch attitudes
+    local compFactor = 1
+    if pitch < 0 then
+        compFactor = SmoothRescale(1.45, -70, 0, -60, 1, pitch)
+    else
+        compFactor = SmoothRescale(1.45, 60, 1, 70, 0, pitch)
+    end
+
+    local P_input_table = {
+        {-1, -Plim + compFactor * neutralPComp},
+        {0,          compFactor * neutralPComp},
+        {1,   Plim + compFactor * neutralPComp},
+    }
+
+    return Table_interpolate(P_input_table, x)
+end
+--========================
+
 FBW.PIDs.phidot = BPPID:new{kp = 0.125, ki = 1, kbp = 0, minout = -30, maxout = 30}
 
 function update()
@@ -13,9 +38,10 @@ function update()
     FBW.PIDs.phidot:backPropagation(PO)
 
     local output = FBW.PIDs.phidot:computePID(
-        fphiDotLimit(Math_clamp(get(TAS_ms) * 1.944, 50, 300)) * get(FCTL_INPUT_X),
-        PhiDot:deg()
+        X_to_P(get(FCTL_INPUT_X)),
+        get(Flightmodel_p_deg)
     )
+
     set(FBW_ROLL_DEF,
         output
     )
